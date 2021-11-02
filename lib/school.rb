588e8aaa34adcb75599155d5e435e7a8e99d73b8
D-Justice <justice.damian96@gmail.com>
2021-11-03 09:25:29 +1100

    Assignment Complete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+    end
+    def grade(num)
+        @roster.collect do |grade, students|
+            if grade == num
+                return students
+            end
+        end
+    end
+    def sort
+        sorted_hash = {}
+        @roster.each do |grade, student|
+            sorted_hash[grade] = student.sort
+        end
+        sorted_hash
+    end
+end
